05aa3e6f21e6f03680b281bb3b9fd6c333b435e0
Shu Sugimoto <shu@su.gimo.to>
2014-10-26 11:45:23 +0900

    implemented message sending via api

--- lib/lita/adapters/hipchat_apisend.rb
+++ lib/lita/adapters/hipchat_apisend.rb
@@ -1,4 +1,5 @@
 require "lita/adapters/hipchat"
+require "hipchat"
 
 # we need to extend Lita core
 class ::Lita::Message
@@ -43,6 +44,63 @@ end
 module Lita
   module Adapters
     class HipchatApisend < ::Lita::Adapters::HipChat
+      require_configs :api_token
+
+      attr_reader :api_client
+
+      def initialize(robot)
+        super robot
+
+        initialize_api
+        fill_jid_to_rooms
+      end
+
+      def send_messages_with_option(target, strings, option: nil)
+        if target.private_message?
+          api_user = api_user_of target.user.id
+          strings.each do |s|
+            api_user.send s # option not supported yet on hipchat-rb
+          end
+        else
+          api_room = api_room_of target.room
+          strings.each do |s|
+            api_room.send "", s, option
+          end
+        end
+      end
+
+      def refresh
+        initialize_api
+        fill_jid_to_rooms
+      end
+
+      private
+
+      def initialize_api
+        @api_client = ::HipChat::Client.new(
+          config.api_token,
+          api_version: config.api_version || "v2",
+          server_url: config.server || "chat.hipchat.com"
+        )
+      end
+
+      def fill_jid_to_rooms
+        api_client.rooms.each do |room|
+          room.xmpp_jid = room.get_room['xmpp_jid']
+        end
+      end
+
+      def api_user_of(jid)
+        api_client.users.select do |user|
+          user.xmpp_jid == jid
+        end.first
+      end
+
+      def api_room_of(jid)
+        api_client.rooms.select do |room|
+          room.xmpp_jid == jid
+        end.first
+      end
     end
 
     Lita.register_adapter(:hipchat_apisend, HipchatApisend)
